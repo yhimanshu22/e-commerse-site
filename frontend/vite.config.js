5cc3173b4f5bb35eee17932d4a1d6ac905737a0a
HIMANSHU YADAV <yhimanshu22@iitk.ac.in>
2024-10-26 16:51:54 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
-import vercel from 'vite-plugin-vercel';
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port:10000,
-  },
-  plugins: [vercel(),react(),tailwindcss()],
+  plugins: [react(),tailwindcss()],
 })
 
-
-
- 
