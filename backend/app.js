c53ae97972a471d855758528e1271e47b4d189cf
HIMANSHU YADAV <yhimanshu22@iitk.ac.in>
2024-10-26 21:15:20 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -6,26 +6,10 @@ import productRoutes from './routes/productRoutes.js';
 dotenv.config();
 const app = express();
 
-const allowedOrigins = [
-    'https://e-commerse-site-seven.vercel.app'
-];
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or Postman)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not ' +
-                        'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    credentials: true // Enable if you are using cookies or authorization headers
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors({
+    origin: 'https://e-commerse-site-seven.vercel.app/',
+    credentials: true
+}));
 
 app.use(express.json());
 app.use('/', productRoutes);
