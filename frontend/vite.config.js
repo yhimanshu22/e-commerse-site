e634cabeaaf27d2f92be1829b2cce85e9f91cfb2
HIMANSHU YADAV <yhimanshu22@iitk.ac.in>
2024-10-26 18:32:20 +0530

    remove proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,6 @@ import tailwindcss from 'tailwindcss';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/': {
-        target: 'https://e-commerse-site-m1a3.onrender.com', // Your backend URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\//, ''), // Remove /api prefix when forwarding
-      },
-    },
-  },
-});
+  plugins: [react(),tailwindcss()],
+})
+
