c4699d6702a103df5049af04033ad08b41b6c787
HIMANSHU YADAV <yhimanshu22@iitk.ac.in>
2024-10-26 16:32:16 +0530

    add vercel plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
+import vercel from 'vite-plugin-vercel';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  server: {
+    port:10000,
+  },
+  plugins: [vercel(),react(),tailwindcss()],
 })
+
+
+
+ 
